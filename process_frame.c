ef7d23dbd17c7c3bbfbc2fb9bc405243e99184f6
oscar <oscar@oscar.(none)>
2016-04-30 10:21:34 +0200

    funktionierende Version.01

--- process_frame.c
+++ process_frame.c
@@ -22,6 +22,9 @@ int TextColor;
 
 float bgrImg[IMG_SIZE];
 
+int16 imgDx[IMG_SIZE];
+int16 imgDy[IMG_SIZE];
+
 const float avgFac = 0.95;
 
 /* skip pixel at border */
@@ -31,130 +34,100 @@ const int Border = 2;
 const int frgLimit = 100;
 
 /* minimum size of objects (sum of all pixels) */
-const int MinArea = 500;
+const int MinArea = 500; //500;
 
 struct OSC_VIS_REGIONS ImgRegions;/* these contain the foreground objects */
 
 void ChangeDetection();
-void SetBackground();
 void Erode_3x3(int InIndex, int OutIndex);
 void Dilate_3x3(int InIndex, int OutIndex);
 void DetectRegions();
 void DrawBoundingBoxes();
+//void getAngleOfRegions();
 
-void ResetProcess()
-{
-	SetBackground();
+void ResetProcess() {
 }
 
-
-void ProcessFrame()
-{
+void ProcessFrame() {
 	//initialize counters
-	if(data.ipc.state.nStepCounter == 1) {
-		SetBackground();
+	if (data.ipc.state.nStepCounter == 1) {
 
 	} else {
 
 		ChangeDetection();
-
 		Erode_3x3(THRESHOLD, INDEX0);
 		Dilate_3x3(INDEX0, THRESHOLD);
-
 		DetectRegions();
-
 		DrawBoundingBoxes();
+		//getAngleOfRegions();
 	}
 }
 
 void ChangeDetection() {
 	int r, c;
-	//set result buffer to zero
+//set result buffer to zero
 	memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
-
-	//loop over the rows
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		//loop over the columns
-		for(c = Border; c < (nc-Border); c++) {
-			float pImg = data.u8TempImage[SENSORIMG][r+c];
-			float pBgr = bgrImg[r+c];
-			float Dif = fabs(pImg-pBgr);
-
-			//if the difference is larger than threshold value (can be changed on web interface)
-			if(Dif > data.ipc.state.nThreshold) {
-				//set pixel value to 255 in BACKGROUND image (only the blue plane)
-				data.u8TempImage[THRESHOLD][r+c] = 255;
-				//increase foreground counter
-				data.u8TempImage[INDEX1][r+c]++;
-				//check whether limit is reached
-				if(data.u8TempImage[INDEX1][r+c] == frgLimit) {
-					//set pixel to background
-					bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
-					data.u8TempImage[INDEX1][r+c] = 0;
-				}
-			} else {
-				// update background image
-				bgrImg[r+c] = avgFac*bgrImg[r+c] + (1-avgFac)*(float) data.u8TempImage[SENSORIMG][r+c];
-				// set value for display
-				data.u8TempImage[BACKGROUND][r+c] = (unsigned char) bgrImg[r+c];
-				//set foreground counter to zero
-				data.u8TempImage[INDEX1][r+c] = 1;
+//loop over the rows
+	for (r = Border * nc; r < (nr - Border) * nc; r += nc) {
+//loop over the columns
+		for (c = Border; c < (nc - Border); c++) {
+			unsigned char* p = &data.u8TempImage[SENSORIMG][r + c];
+			/* implement Sobel filter in x-direction */
+			int32 dx = -(int32) *(p - nc - 1) + (int32) *(p - nc + 1)
+					- 2 * (int32) *(p - 1) + 2 * (int32) *(p + 1)
+					- (int32) *(p + nc - 1) + (int32) *(p + nc + 1);
+			int32 dy = -(int32) *(p - nc - 1) - 2 * (int32) *(p - nc)
+					- (int32) *(p - nc + 1) + (int32) *(p + nc - 1)
+					+ 2 * (int32) *(p + nc) + (int32) *(p + nc + 1);
+			/* check if norm is larger than threshold */
+			int32 df2 = dx * dx + dy * dy;
+			int32 thr2 = data.ipc.state.nThreshold * data.ipc.state.nThreshold;
+			if (df2 > thr2) { //avoid square root
+				//set pixel value to 255 in THRESHOLD image for gui
+				data.u8TempImage[THRESHOLD][r + c] = 255;
 			}
+			//store derivatives (int16 is enough)
+			imgDx[r + c] = (int16) dx;
+			imgDy[r + c] = (int16) dy;
+			//possibility to visualize data
+			data.u8TempImage[BACKGROUND][r + c] = (uint8) MAX(0,
+					MIN(255, 128+dx));
 		}
 	}
 }
 
-
-void SetBackground() {
-	int r, c;
-
-	//loop over the rows
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		//loop over the columns
-		for(c = Border; c < (nc-Border); c++) {
-			bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
-		}
-	}
-	//set all counters to zero
-	memset(data.u8TempImage[INDEX1], 0, IMG_SIZE);
-}
-
-
-void Erode_3x3(int InIndex, int OutIndex)
-{
+void Erode_3x3(int InIndex, int OutIndex) {
 	int c, r;
 
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		for(c = Border; c < (nc-Border); c++) {
-			unsigned char* p = &data.u8TempImage[InIndex][r+c];
-			data.u8TempImage[OutIndex][r+c] = *(p-nc-1) & *(p-nc) & *(p-nc+1) &
-											   *(p-1)    & *p      & *(p+1)    &
-											   *(p+nc-1) & *(p+nc) & *(p+nc+1);
+	for (r = Border * nc; r < (nr - Border) * nc; r += nc) {
+		for (c = Border; c < (nc - Border); c++) {
+			unsigned char* p = &data.u8TempImage[InIndex][r + c];
+			data.u8TempImage[OutIndex][r + c] = *(p - nc - 1) & *(p - nc)
+					& *(p - nc + 1) & *(p - 1) & *p & *(p + 1) & *(p + nc - 1)
+					& *(p + nc) & *(p + nc + 1);
 		}
 	}
 }
 
-void Dilate_3x3(int InIndex, int OutIndex)
-{
+void Dilate_3x3(int InIndex, int OutIndex) {
 	int c, r;
 
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		for(c = Border; c < (nc-Border); c++) {
-			unsigned char* p = &data.u8TempImage[InIndex][r+c];
-			data.u8TempImage[OutIndex][r+c] = *(p-nc-1) | *(p-nc) | *(p-nc+1) |
-											        *(p-1)    | *p      | *(p+1)    |
-											        *(p+nc-1) | *(p+nc) | *(p+nc+1);
+	for (r = Border * nc; r < (nr - Border) * nc; r += nc) {
+		for (c = Border; c < (nc - Border); c++) {
+			unsigned char* p = &data.u8TempImage[InIndex][r + c];
+			data.u8TempImage[OutIndex][r + c] = *(p - nc - 1) | *(p - nc)
+					| *(p - nc + 1) | *(p - 1) | *p | *(p + 1) | *(p + nc - 1)
+					| *(p + nc) | *(p + nc + 1);
 		}
 	}
 }
-
 
 void DetectRegions() {
 	struct OSC_PICTURE Pic;
 	int i;
 
 	//set pixel value to 1 in INDEX0 because the image MUST be binary (i.e. values of 0 and 1)
-	for(i = 0; i < IMG_SIZE; i++) {
+	for (i = 0; i < IMG_SIZE; i++) {
 		data.u8TempImage[INDEX0][i] = data.u8TempImage[THRESHOLD][i] ? 1 : 0;
 	}
 
@@ -165,17 +138,125 @@ void DetectRegions() {
 	Pic.type = OSC_PICTURE_BINARY;
 
 	//now do region labeling and feature extraction
-	OscVisLabelBinary( &Pic, &ImgRegions);
-	OscVisGetRegionProperties( &ImgRegions);
+	OscVisLabelBinary(&Pic, &ImgRegions);
+	OscVisGetRegionProperties(&ImgRegions);
 }
 
-
 void DrawBoundingBoxes() {
+	int a45Counter = 0;
+	int a90Counter = 0;
+	int a135Counter = 0;
+	int a0Counter = 0;
 	uint16 o;
-	for(o = 0; o < ImgRegions.noOfObjects; o++) {
-		if(ImgRegions.objects[o].area > MinArea) {
-			DrawBoundingBox(ImgRegions.objects[o].bboxLeft, ImgRegions.objects[o].bboxTop,
-							ImgRegions.objects[o].bboxRight, ImgRegions.objects[o].bboxBottom, false, GREEN);
+	for (o = 0; o < ImgRegions.noOfObjects; o++) {
+		if (ImgRegions.objects[o].area > MinArea) {
+			DrawBoundingBox(ImgRegions.objects[o].bboxLeft,
+					ImgRegions.objects[o].bboxTop,
+					ImgRegions.objects[o].bboxRight,
+					ImgRegions.objects[o].bboxBottom, false, GREEN);
+
+			//bestimme den Winkel
+			struct OSC_VIS_REGIONS_RUN* currentRun = ImgRegions.objects[o].root;
+			//loop over runs of current object
+			do {
+				//loop over pixel of current run
+				int c;
+				for (c = currentRun->startColumn; c <= currentRun->endColumn;
+						c++) {
+					int r = currentRun->row;
+					//processing for individual pixel at row r and column c
+					double angle = 3.14+atan2(imgDy[r * nc + c], imgDx[r * nc + c]);
+					if (angle>3.14){
+						angle=angle-3.14;
+					}
+					if (angle > 0.1) {
+						if (angle > 0.3927 && angle <= 1.178) {
+							a45Counter++;
+						} else if (angle > 1.178 && angle <= 1.964) {
+							a90Counter++;
+						} else if (angle > 1.964 && angle <= 2.749) {
+							a135Counter++;
+						} else {
+							a0Counter++;
+						}
+					}
+				}
+			currentRun = currentRun->next; //get net run of current object
+		} while (currentRun != NULL); //end of current object
+		if (a0Counter > a90Counter && a0Counter > a135Counter&& a0Counter > a45Counter) {
+				DrawString(ImgRegions.objects[o].centroidX,
+						ImgRegions.objects[o].centroidY, 5, LARGE, BLUE,"0 deg");
+			}
+			if (a45Counter > a90Counter && a45Counter > a135Counter
+					&& a45Counter > a0Counter) {
+				DrawString(ImgRegions.objects[o].centroidX,
+						ImgRegions.objects[o].centroidY, 6, LARGE, BLUE,"45 deg");
+			}
+			if (a90Counter > a0Counter && a90Counter > a45Counter
+					&& a90Counter > a135Counter) {
+				DrawString(ImgRegions.objects[o].centroidX,ImgRegions.objects[o].centroidY, 6, LARGE, BLUE,"90 deg");
+			}
+			if (a135Counter > a0Counter && a135Counter > a45Counter&& a135Counter > a90Counter) {
+			DrawString(ImgRegions.objects[o].centroidX,ImgRegions.objects[o].centroidY,7,LARGE,BLUE,"135 deg");
+		}
+		a0Counter = 0;
+		a90Counter = 0;
+		a135Counter = 0;
+		a45Counter = 0;
 		}
 	}
 }
+
+/*void getAngleOfRegions() {
+//loop over objects
+int o;
+for (o = 0; o < ImgRegions.noOfObjects; o++) {
+	//get pointer to root run of current object
+	struct OSC_VIS_REGIONS_RUN* currentRun = ImgRegions.objects[o].root;
+	//loop over runs of current object
+	do {
+		//loop over pixel of current run
+		int a45Counter = 0;
+		int a90Counter = 0;
+		int a135Counter = 0;
+		int a0Counter = 0;
+		int c;
+		for (c = currentRun->startColumn; c <= currentRun->endColumn; c++) {
+			int r = currentRun->row;
+			//processing for individual pixel at row r and column c
+			double angle = atan2(imgDy[r * nc + c], imgDx[r * nc + c]);
+			if (angle != 0) {
+				if (angle > 0.3927 && angle <= 1.178) {
+					a45Counter++;
+				} else if (angle > 1.78 && angle <= 1.964) {
+					a90Counter++;
+				} else if (angle > 1.964 && angle <= 2.749) {
+					a135Counter++;
+				} else {
+					a0Counter++;
+				}
+			}
+
+		}
+		if (a0Counter > a90Counter && a0Counter > a135Counter && a0Counter > a45Counter) {
+			DrawString(ImgRegions.objects[o].centroidX,ImgRegions.objects[o].centroidY, 5, LARGE, BLUE, "0 deg");
+		}
+		else if (a45Counter > a90Counter && a45Counter > a135Counter && a45Counter > a0Counter) {
+			DrawString(ImgRegions.objects[o].centroidX,ImgRegions.objects[o].centroidY, 6, LARGE, BLUE, "45 deg");
+		}
+		else if (a90Counter > a0Counter && a90Counter > a45Counter && a90Counter > a135Counter) {
+			DrawString(ImgRegions.objects[o].centroidX,ImgRegions.objects[o].centroidY, 6, LARGE, BLUE, "90 deg");
+		}
+		else if (a135Counter > a0Counter && a135Counter > a45Counter && a135Counter > a90Counter) {
+			DrawString(ImgRegions.objects[o].centroidX,ImgRegions.objects[o].centroidY,7,LARGE,BLUE,"135 deg");
+	}
+	a0Counter = 0;
+	a90Counter = 0;
+	a135Counter = 0;
+	a45Counter = 0;
+	currentRun = currentRun->next; //get net run of current object
+} while (currentRun != NULL); //end of current object
+}
+
+}*/
+
